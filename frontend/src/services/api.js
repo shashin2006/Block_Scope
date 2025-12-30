249af9a11ae19b9acf36d3be2fc05ca4f102043b
Shashin R <shashinrajkumar@gmail.com>
2025-12-30 21:12:00 +0530

    deployment changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3002';
+const API_BASE_URL = 'https://block-scope-1.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
